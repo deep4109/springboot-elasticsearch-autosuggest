c07a3b2a9e2aaaa042370623c9ce71b4e38d1044
deep4109 <singhdeepankar41@gmail.com>
2025-08-10 14:18:20 +0530

    first commit

---
+++ src/main/java/com/example/springbootautosuggestusingelasticsearch/SpringBootAutoSuggestUsingElasticsearchApplication.java
@@ -0,0 +1,30 @@
+package com.example.springbootautosuggestusingelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoSuggestUsingElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAutoSuggestUsingElasticsearchApplication.class, args);
+    }
+    //git comments
+
+
+    /**
+     * Just a simple method to write some notes
+     * <p>
+     * 1 - create a edge-ngram filter
+     * and edge-ngram analyzer on the index[product]
+     */
+    public static void forSearchAutocomplete(){
+        // Method implementation will go here
+    }
+
+
+
+
+
+
+}
